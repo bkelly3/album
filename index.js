326f53c09bc6b41b24a80f64a75df9a6d51e5d4c
Bill Kelly (CORE SERVICES ENGINEERING) <wkelly@microsoft.com>
2018-02-27 13:09:18 -0800

    index change

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+//Import a library to help create a component
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
 
-AppRegistry.registerComponent('album', () => App);
+//Create a component
+const App = () => (
+        <Text>Some Text</Text>
+);
+
+//Render the component to a device
+AppRegistry.registerComponent('album', () => App)
