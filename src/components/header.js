288cba68a0ce2d697308afcfd6b19435c0ed6592
Bill Kelly (CORE SERVICES ENGINEERING) <wkelly@microsoft.com>
2018-02-28 13:55:35 -0800

    update

--- src/components/header.js
+++ src/components/header.js
@@ -2,34 +2,34 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-//make a component
+// make a component
 const Header = (props) => {
-    const { textStyle, viewStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-    return (
+  return (
     <View style={viewStyle}>
-        <Text style={textStyle}>{props.headerText}</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
-    );
+  );
 };
 
 const styles = {
-    viewStyle: {
-        backgroundColor: '#F8F8F8',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 60,
-        paddingTop: 15,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2},
-        shadowOpacity: 0.2,
-        elevation: 2,
-        position: 'relative'
-    },
-    textStyle: {
-        fontSize: 20
-    }
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
 };
 
-//make the component available to other parts of the app
+// make the component available to other parts of the app
 export default Header;
