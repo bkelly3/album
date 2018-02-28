09448063ae74a583e7297a8509035d5986d46cc9
Bill Kelly (CORE SERVICES ENGINEERING) <wkelly@microsoft.com>
2018-02-28 14:13:20 -0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {AppRegistry, View} from 'react-native.js';
 import Header from './src/components/header.js';
 import AlbumList from './src/components/AlbumList.js';
+
 // Create a component
 const App = () => (
   <View>
